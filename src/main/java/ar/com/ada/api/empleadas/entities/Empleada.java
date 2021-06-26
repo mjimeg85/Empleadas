f5b103389f27536f3aec782e914988af7b25db74
mjimeg85 <mjimeg85@gmail.com>
2021-06-27 01:30:17 +0200

    modificacion de estado_id y llenado de application properties

--- src/main/java/ar/com/ada/api/empleadas/entities/Empleada.java
+++ src/main/java/ar/com/ada/api/empleadas/entities/Empleada.java
@@ -22,6 +22,8 @@ public class Empleada {
     @JoinColumn(name = "categoria_id", referencedColumnName = "categoria_id")
     private Categoria categoria;
     private BigDecimal sueldo;
+
+    @Column(name="estado_id")
     private int estado;
 
     @Column(name="fecha_alta")
